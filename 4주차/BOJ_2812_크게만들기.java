4b5ca186d1afda50decfeb0e5845ab49152edb86
Gym-Mentor <61284215+Gym-Mentor@users.noreply.github.com>
2021-02-22 20:37:49 +0900

    4주차-3 그리디 BOJ_2812_크게만들기

--- 4주차/BOJ_2812_크게만들기.java
+++ 4주차/BOJ_2812_크게만들기.java
@@ -1,45 +1,66 @@
-package boj_2812;
+package boj_2448;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-
-	static int N,K;
-	static char arr[];
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-
+	static int N;
+	static char arr[][];
+	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st= new StringTokenizer(br.readLine());
+		StringBuilder sb=new StringBuilder();
+		N=Integer.parseInt(br.readLine());
+		arr=new char[N][N*2];
 		
-		N=Integer.parseInt(st.nextToken());
-		K=Integer.parseInt(st.nextToken());
-		arr= new char[N];
-		arr=br.readLine().toCharArray();
-		Deque<Character> dq= new ArrayDeque<>();
-		
-		for(int i=0;i<N;i++)
+		for(char[] rows:arr)
 		{
-			while(K>0&&!dq.isEmpty()&&dq.peekLast()<arr[i])
-			{
-				dq.pollLast();
-				K--;
-			}
-			dq.offerLast(arr[i]);
-			
+			Arrays.fill(rows, ' ');
 		}
 		
-		int size=dq.size();
-		for(int i=0;i<size-K;i++)
-		System.out.print(dq.pollFirst());
+		recursive(0,N-1,N);
+		for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<N*2;j++)
+			{
+				sb.append(arr[i][j]);
+			}
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-
+	
+	static void recursive(int h,int w,int n)
+	{
+		if(n==3)
+		{
+			//첫 째줄
+			arr[h][w]='*';
+			
+			//두 째줄
+			arr[h+1][w-1]='*';
+			arr[h+1][w+1]='*';
+			
+			//세 째줄
+			arr[h+2][w-2]='*';
+			arr[h+2][w-1]='*';
+			arr[h+2][w]='*';
+			arr[h+2][w+1]='*';
+			arr[h+2][w+2]='*';
+			return;
+		}
+		// 하나의 삼각형은 총 3개의 삼각형으로 분해됨
+		// 각 삼각형의 젤 위의 좌표로 재귀호출
+		recursive(h,w,n/2);
+		recursive(h+n/2,w-n/2,n/2);
+		recursive(h+n/2,w+n/2,n/2);
+		
+	}
 }
