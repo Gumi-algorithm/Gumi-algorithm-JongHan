4e17b24c5756d8d3ab7411ddf11e7343be2bb31a
Gym-Mentor <61284215+Gym-Mentor@users.noreply.github.com>
2021-05-14 22:00:55 +0900

    16주차-1  boj 15486 퇴사2

--- 16주차/Main.java
+++ 16주차/Main.java
@@ -1,40 +1,33 @@
-package boj_7490;
-
+package boj_15486;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int T,N;
-	static int[] arr;
+	static int N,max=0;
+	static int DP[],cost[],earn[];
 	public static void main(String[] args) throws IOException{
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		T=Integer.parseInt(br.readLine());
-		for(int tc=1;tc<=T;tc++)
+		
+		N=Integer.parseInt(br.readLine());
+		DP=new int[N+2];
+		cost=new int[N+2];
+		earn=new int[N+2];
+		for(int i=1;i<=N;i++)
 		{
-			N=Integer.parseInt(br.readLine());
-			comb(0,1,1,1,"1");
-			System.out.println();
+			StringTokenizer st= new StringTokenizer(br.readLine());
+			cost[i]=Integer.parseInt(st.nextToken());
+			earn[i]=Integer.parseInt(st.nextToken());
+			max=Math.max(max, DP[i]);
+			if(cost[i]+i<=N+1)
+			{
+				DP[i+cost[i]]=Math.max(max+earn[i], DP[i+cost[i]]);
+			}
 		}
+		max=Math.max(max, DP[N+1]);
+		System.out.println(max);
 	}
 
-	static void comb(int sum,int sign, int num, int n, String str)
-	{
-		if(n==N)
-		{
-			sum+=(num*sign);
-			if(sum==0)
-			{
-				System.out.println(str);
-			}
-			return;
-		}
-		else
-		{
-			comb(sum,sign,num*10+(n+1),n+1,str+' '+(n+1));
-			comb(sum+(sign*num),1,(n+1),n+1,str+'+'+(n+1));
-			comb(sum+(sign*num),-1,(n+1),n+1,str+'-'+(n+1));
-		}
-	}
 }
